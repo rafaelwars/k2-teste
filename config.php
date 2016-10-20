d58c41dbce51859ead40254b496997ba777cd518
rafaelwars <rafaelwars@gmail.com>
2016-10-20 17:40:56 -0200

    Commit
    Comittar

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/** O nome do banco de dados*/
+define('DB_NAME', 'teste1');
+/** Usuário do banco de dados MySQL */
+define('DB_USER', 'admin');
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', '1234');
+/** nome do host do MySQL */
+define('DB_HOST', 'localhost');
+/** caminho absoluto para a pasta do sistema **/
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+	
+/** caminho no server para o sistema **/
+if ( !defined('BASEURL') )
+	define('BASEURL', '/teste-1/');
+	
+/** caminho do arquivo de banco de dados **/
+if ( !defined('DBAPI') )
+	define('DBAPI', ABSPATH . 'inc/database.php');
+
+
+/** caminhos dos templates de header e footer **/
+define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
+define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
---
+++ customers/functions.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../config.php');
+require_once(DBAPI);
+$customers = null;
+$customer = null;
+/**
+ *  Listagem de Clientes
+ */
+function index() {
+	global $customers;
+	$customers = find_all('customers');
+}
+function add() {
+  if (!empty($_POST['customer'])) {
+    
+    $today = 
+      date_create('now', new DateTimeZone('America/Sao_Paulo'));
+    $customer = $_POST['customer'];
+    $customer['modified'] = $customer['created'] = $today->format("Y-m-d H:i:s");
+    
+    save('customers', $customer);
+    header('location: index.php');
+  }
+}
